5fdbbfedf098bfa534c9562b6bd7cec9a2ddabd1
Sebastian Nuñez <sebas11np@gmail.com>
2026-03-20 20:59:34 -0600

    fix: cambio en código 2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://crud-app-latest.onrender.com/',
+    origin: 'https://crud-app-latest.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
